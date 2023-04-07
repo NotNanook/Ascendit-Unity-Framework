f6831479f1de234176de5e04b15a68129cf8fbf2
NotNanook <gogabrys@gmail.com>
2023-04-07 20:21:23 +0200

    primitive esp code

--- Ascendit-Unity-Framework/cheats/cheats/ExampleCheat.cpp
+++ Ascendit-Unity-Framework/cheats/cheats/ExampleCheat.cpp
@@ -4,12 +4,66 @@
 
 ExampleCheat exampleCheat;
 
+std::vector<Vector3> playerPosList;
+Camera* camera;
+
+int getAlivePlayerCount(MirrorClientObject* buffer[]) {
+	int count = 0;
+	for (int i = 0; i < 64; i++) {
+		count++;
+		if (buffer[i] == 0x0000000000000000) {
+			return count;
+		}
+	}
+	return count;
+}
+
 void ExampleCheat::onRenderUpdate() {
+	List<MirrorClientObject*>* playerList = Functions::getAlivePlayers(NULL, NULL);
+	int alivePlayerCount = getAlivePlayerCount(playerList->content->buffer);
+
+	camera = Functions::getCamera();
+	playerPosList.clear();
+
+	for (int i = 0; i < alivePlayerCount - 1; i++) {
+		MirrorClientObject* playerOne = playerList->content->buffer[i];
+		ClientPlayerInstance* clientPlayerInstance = Functions::getClientPlayer(playerOne);
+		CharacterObject* characterObject = Functions::getCharacterObject(clientPlayerInstance);
+		Transform* transform = Functions::getTransform(*characterObject);
+
+		Vector3 out;
+		Functions::getPosition(transform, &out);
+		playerPosList.push_back(out);
+	}
+
 	ImDrawList* drawList = ImGui::GetBackgroundDrawList();
-	drawList->AddLine(ImVec2(ImGui::GetIO().DisplaySize.x / 2, ImGui::GetIO().DisplaySize.y), ImVec2(ImGui::GetIO().DisplaySize.x / 2, ImGui::GetIO().DisplaySize.y / 2), ImColor(255, 255, 255, 255), 2.0f);
+	for (Vector3 vec : playerPosList) {
+		Vector3 screenPos = Functions::worldToScreenPoint(camera, &vec);
+		if (screenPos.z > 0) {
+			screenPos.y = (ImGui::GetIO().DisplaySize.y - screenPos.y);
+		}
+
+		drawList->AddLine(ImVec2(ImGui::GetIO().DisplaySize.x / 2, ImGui::GetIO().DisplaySize.y / 2), ImVec2(screenPos.x, screenPos.y), ImColor(255, 255, 255, 127), 1.0f);
+	}
 }
 
 void ExampleCheat::onEnable() {
-	std::vector<MirrorClientObject*>* playerList = Functions::getAlivePlayers(NULL, NULL);
-	std::cout << playerList << std::endl;
+	/*
+	List<MirrorClientObject*>* playerList = Functions::getAlivePlayers(NULL, NULL);
+	int alivePlayerCount = getAlivePlayerCount(playerList->content->buffer);
+
+	camera = Functions::getCamera();
+	playerPosList.clear();
+
+	for (int i = 0; i < alivePlayerCount - 1; i++) {
+		MirrorClientObject* playerOne = playerList->content->buffer[i];
+		ClientPlayerInstance* clientPlayerInstance = Functions::getClientPlayer(playerOne);
+		CharacterObject* characterObject = Functions::getCharacterObject(clientPlayerInstance);
+		Transform* transform = Functions::getTransform(*characterObject);
+
+		Vector3 out;
+		Functions::getPosition(transform, &out);
+		playerPosList.push_back(out);
+	}
+	*/
 }
